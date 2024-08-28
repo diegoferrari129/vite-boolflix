adf98312648c059c0fe0258434cd4f4caf3595cb
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-08-28 16:36:03 +0200

    [UPD] replaced and updated 'roundedVote' methods with 'getStars'  and updated template with a conditional statement to show full or empty stars in AppMain.vue component"

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,11 +9,12 @@ export default {
     },
 
     methods: {
-        roundedVoteMovie(movie) {
-            return Math.round(movie.vote_average / 2);
-        },
-        roundedVoteShow(show) {
-            return Math.round(show.vote_average / 2);
+        getStars(vote) {
+            const stars = [];
+            for (let i = 0; i < 5; i++) {
+                stars.push(i < Math.round(vote / 2) ? 'filled' : 'empty');
+            }
+            return stars;
         }
     }
 }   
@@ -25,19 +26,33 @@ export default {
          <!-- movies -->
         <li v-for="movie in store.filteredMovies" :key="movie.id">
             <img :src="`https://image.tmdb.org/t/p/w342/${movie.poster_path}`">
+            
             {{ movie.title }}
+
             {{ movie.original_title }}
+
             <img :src="`/public/flags/${movie.original_language}.png` " />
-            {{ roundedVoteMovie(movie) }}
+
+            <span v-for="star, index in getStars(movie.vote_average)" :key="index">
+                <i v-if="star === 'filled'" class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                <i v-if="star === 'empty'" class="fa-solid fa-star" style="color: #c0c0c0;"></i>
+            </span>
         </li>
         
         <!-- TV series -->
         <li v-for="show in store.filteredSeries" :key="show.id">
             <img :src="`https://image.tmdb.org/t/p/w342/${show.poster_path}`">
+
             {{ show.name }}
+
             {{ show.original_name }}
+
             <img :src="`/public/flags/${show.original_language}.png`" />
-            {{ roundedVoteShow(show) }}
+
+            <span v-for="star, index in getStars(show.vote_average)" :key="index">
+                <i v-if="star === 'filled'" class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                <i v-if="star === 'empty'" class="fa-solid fa-star" style="color: #c0c0c0;"></i>
+            </span>
         </li>
     </ul>
 </template>
