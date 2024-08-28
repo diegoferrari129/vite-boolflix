7e56c1b5b7153e14888d9547d9245687a07759d9
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-08-28 23:41:12 +0200

    [UPD] added hover effects

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -28,26 +28,34 @@ export default {
                     <img :src="`https://image.tmdb.org/t/p/w342/${movie.poster_path}`" class="card-img">
                     <div class="card-img-overlay">
                         <h5 class="card-title">
-                            Title:
+                            <span class="fw-bolder">
+                                Title:
+                            </span>
                             {{ movie.title }}
                         </h5>
                         <h6 class="card-title">
-                            Original Title:
+                            <span class="fw-bolder">
+                                Original Title:
+                            </span>
                             {{ movie.original_title }}
                         </h6>
                         <div class="card-text">
-                            <span>Lang:</span>
+                            <span class="fw-bolder">Lang:</span>
                             <img :src="`/public/flags/${movie.original_language}.png`" class="ps-2" />
                         </div>
                         <div class="card-text mt-1">
-                            Rating:
+                            <span class="fw-bolder">
+                                Rating:
+                            </span>
                             <span v-for="star, index in getStars(movie.vote_average)" :key="index">
                                 <i v-if="star === 'filled'" class="fa-solid fa-star" style="color: #FFD43B;"></i>
                                 <i v-if="star === 'empty'" class="fa-solid fa-star" style="color: #c0c0c0;"></i>
                             </span>
                         </div>
                         <div class="card-text mt-2">
-                            Overview:
+                            <span class="fw-bolder">
+                                Overview:
+                            </span>
                             {{ movie.overview }}
                         </div>
                     </div>
@@ -63,26 +71,36 @@ export default {
                     <img :src="`https://image.tmdb.org/t/p/w342/${show.poster_path}`" class="card-img">
                     <div class="card-img-overlay">
                         <h5 class="card-title">
-                            Title:
+                            <span class="fw-bolder">
+                                Title:
+                            </span>
                             {{ show.name }}
                         </h5>
                         <h6 class="card-title">
-                            Original Title:
+                            <span class="fw-bolder">
+                                Original Title:
+                            </span>
                             {{ show.original_name }}
                         </h6>
                         <div class="card-text">
-                            <span>Lang:</span>
+                            <span class="fw-bolder">
+                                Lang:
+                            </span>
                             <img :src="`/public/flags/${show.original_language}.png`" class="ps-2" />
                         </div>
                         <div class="card-text mt-1">
-                            Rating:
+                            <span class="fw-bolder">
+                                Rating:
+                            </span>
                             <span v-for="star, index in getStars(show.vote_average)" :key="index">
                                 <i v-if="star === 'filled'" class="fa-solid fa-star" style="color: #FFD43B;"></i>
                                 <i v-if="star === 'empty'" class="fa-solid fa-star" style="color: #c0c0c0;"></i>
                             </span>
                         </div>
                         <div class="card-text mt-2">
-                            Overview:
+                            <span class="fw-bolder">
+                                Overview:
+                            </span>
                             {{ show.overview }}
                         </div>
                     </div>
@@ -94,5 +112,22 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-    
+
+    .card-img-overlay {
+        opacity: 0;
+        transition: opacity 0.5s;
+    }
+    .card {
+
+        &:hover {
+            
+            .card-img-overlay {
+                opacity: 1;
+            }
+            .card-img {
+                display: none;
+            }
+        }
+    }
+
 </style>
