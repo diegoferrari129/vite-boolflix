d9d4fa857147df1a6a1443efd50373c281fbd529
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-08-28 22:44:31 +0200

    [UPD] created new component Cards.vue and added style

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,4 +1,5 @@
 <script>
+import Cards from './Cards.vue';
 import { store } from '../store.js';
 
 export default {
@@ -8,54 +9,20 @@ export default {
         }
     },
 
-    methods: {
-        getStars(vote) {
-            const stars = [];
-            for (let i = 0; i < 5; i++) {
-                stars.push(i < Math.round(vote / 2) ? 'filled' : 'empty');
-            }
-            return stars;
-        }
-    }
+    components: {
+        Cards,
+    },
 }   
 </script>
 
 <template>
-    <ul>
-        <!-- CICLO PER MOSTRARE I VALORI FILTRATI DELLA CHIAMATA API -->
-         <!-- movies -->
-        <li v-for="movie in store.movies" :key="movie.id">
-            <img :src="`https://image.tmdb.org/t/p/w342/${movie.poster_path}`">
-            
-            {{ movie.title }}
-
-            {{ movie.original_title }}
-
-            <img :src="`/public/flags/${movie.original_language}.png` " />
-
-            <span v-for="star, index in getStars(movie.vote_average)" :key="index">
-                <i v-if="star === 'filled'" class="fa-solid fa-star" style="color: #FFD43B;"></i>
-                <i v-if="star === 'empty'" class="fa-solid fa-star" style="color: #c0c0c0;"></i>
-            </span>
-        </li>
-        
-        <!-- TV series -->
-        <li v-for="show in store.series" :key="show.id">
-            <img :src="`https://image.tmdb.org/t/p/w342/${show.poster_path}`">
-
-            {{ show.name }}
-
-            {{ show.original_name }}
-
-            <img :src="`/public/flags/${show.original_language}.png`" />
-
-            <span v-for="star, index in getStars(show.vote_average)" :key="index">
-                <i v-if="star === 'filled'" class="fa-solid fa-star" style="color: #FFD43B;"></i>
-                <i v-if="star === 'empty'" class="fa-solid fa-star" style="color: #c0c0c0;"></i>
-            </span>
-        </li>
-    </ul>
+    <main>
+        <Cards />
+    </main>
 </template>
 
 <style lang="scss" scoped>
+    main {
+        background-color: #434343;
+    }
 </style>
---
+++ src/components/Cards.vue
@@ -0,0 +1,98 @@
+<script>
+import { store } from '../store.js';
+
+export default {
+    data() {
+        return {
+            store
+        }
+    },
+
+    methods: {
+        getStars(vote) {
+            const stars = [];
+            for (let i = 0; i < 5; i++) {
+                stars.push(i < Math.round(vote / 2) ? 'filled' : 'empty');
+            }
+            return stars;
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row"> 
+            <div class="col-4" v-for="movie in store.movies" :key="movie.id">
+                <div class="card bg-dark text-white my-3">
+                    <img :src="`https://image.tmdb.org/t/p/w342/${movie.poster_path}`" class="card-img">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">
+                            Title:
+                            {{ movie.title }}
+                        </h5>
+                        <h6 class="card-title">
+                            Original Title:
+                            {{ movie.original_title }}
+                        </h6>
+                        <div class="card-text">
+                            <span>Lang:</span>
+                            <img :src="`/public/flags/${movie.original_language}.png`" class="ps-2" />
+                        </div>
+                        <div class="card-text mt-1">
+                            Rating:
+                            <span v-for="star, index in getStars(movie.vote_average)" :key="index">
+                                <i v-if="star === 'filled'" class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                                <i v-if="star === 'empty'" class="fa-solid fa-star" style="color: #c0c0c0;"></i>
+                            </span>
+                        </div>
+                        <div class="card-text mt-2">
+                            Overview:
+                            {{ movie.overview }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row"> 
+            <div class="col-4" v-for="show in store.series" :key="show.id">
+                <div class="card bg-dark text-white my-3">
+                    <img :src="`https://image.tmdb.org/t/p/w342/${show.poster_path}`" class="card-img">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">
+                            Title:
+                            {{ show.name }}
+                        </h5>
+                        <h6 class="card-title">
+                            Original Title:
+                            {{ show.original_name }}
+                        </h6>
+                        <div class="card-text">
+                            <span>Lang:</span>
+                            <img :src="`/public/flags/${show.original_language}.png`" class="ps-2" />
+                        </div>
+                        <div class="card-text mt-1">
+                            Rating:
+                            <span v-for="star, index in getStars(show.vote_average)" :key="index">
+                                <i v-if="star === 'filled'" class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                                <i v-if="star === 'empty'" class="fa-solid fa-star" style="color: #c0c0c0;"></i>
+                            </span>
+                        </div>
+                        <div class="card-text mt-2">
+                            Overview:
+                            {{ show.overview }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
